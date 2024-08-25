7c1cfe52f5bbadb2623aa15281da3c822492943f
NainooG <khush.nainn@gmail.com>
2024-08-24 23:28:23 -0700

    good

--- frontend/src/components/Legend.jsx
+++ frontend/src/components/Legend.jsx
@@ -10,36 +10,38 @@ const Legend = () => {
         visibility: 'visible',
         zIndex: 1000,
         color: 'black',
-        width: '250px',  // Increased width for more space
+        width: '300px',  // Increased width for more space
       }}
     >
       <div className="text-center mb-4 font-bold text-lg" style={{ color: 'black' }}>Bivariate Legend</div>
       
-      <div className="flex flex-col items-center">
+      <div className="flex items-center justify-between">
+        {/* Y-axis label on the left */}
+        <div className="flex flex-col justify-between text-sm" style={{ textAlign: 'right', marginRight: '12px' }}>
+          <span style={{ marginBottom: '26px', marginTop: '8px' }}>High % POC</span>
+          <span style={{ marginTop: '26px' }}>Low % POC</span>
+        </div>
+        
         {/* Color grid */}
-        <div className="grid grid-cols-3 grid-rows-3 gap-1">
-          <div className="bg-[#3b4994] w-8 h-8"></div>
-          <div className="bg-[#8c62aa] w-8 h-8"></div>
-          <div className="bg-[#be64ac] w-8 h-8"></div>
-          <div className="bg-[#5698b9] w-8 h-8"></div>
-          <div className="bg-[#a5add3] w-8 h-8"></div>
-          <div className="bg-[#dfb0d6] w-8 h-8"></div>
-          <div className="bg-[#5ac8c8] w-8 h-8"></div>
-          <div className="bg-[#ace4e4] w-8 h-8"></div>
-          <div className="bg-[#e8e8e8] w-8 h-8"></div>
-        </div>
+        <div className="flex flex-col">
+          <div className="grid grid-cols-3 grid-rows-3 gap-0">
+            <div className="bg-[#3b4994] w-10 h-10"></div>
+            <div className="bg-[#8c62aa] w-10 h-10"></div>
+            <div className="bg-[#be64ac] w-10 h-10"></div>
+            <div className="bg-[#5698b9] w-10 h-10"></div>
+            <div className="bg-[#a5add3] w-10 h-10"></div>
+            <div className="bg-[#dfb0d6] w-10 h-10"></div>
+            <div className="bg-[#5ac8c8] w-10 h-10"></div>
+            <div className="bg-[#ace4e4] w-10 h-10"></div>
+            <div className="bg-[#e8e8e8] w-10 h-10"></div>
+          </div>
 
-        {/* X-axis label */}
-        <div className="flex justify-between w-full mt-2">
-          <span className="text-sm" style={{ color: 'black' }}>Low % POC</span>
-          <span className="text-sm" style={{ color: 'black' }}>High % POC</span>
+          {/* X-axis label below the grid */}
+          <div className="flex justify-between w-full mt-2">
+            <span className="text-sm" style={{ marginRight: '50px', color: 'black' }}>Low Access</span>
+            <span className="text-sm" style={{ color: 'black' }}>High Access</span>
+          </div>
         </div>
-      </div>
-
-      {/* Y-axis label */}
-      <div className="flex justify-between w-full mt-2">
-        <span className="text-sm" style={{ color: 'black' }}>Low Access</span>
-        <span className="text-sm" style={{ color: 'black' }}>High Access</span>
       </div>
     </div>
   );
