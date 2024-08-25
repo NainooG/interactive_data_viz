dce5df4d9b3558d00703f0a4c802e46efc7b56b1
NainooG <khush.nainn@gmail.com>
2024-08-24 23:43:50 -0700

    done

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center">
       <header className="w-full bg-gray-800 text-white p-4 text-center">
-        <h1 className="text-3xl font-bold">Interactive Map with Bivariate Legend</h1>
+        <h1 className="text-3xl font-bold">% People of Color and Limited Access To Healthy Foods in Washington State</h1>
       </header>
 
       <main className="flex-1 w-full flex justify-center items-center">
